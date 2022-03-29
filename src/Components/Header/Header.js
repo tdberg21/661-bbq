e935b87eef692aa1d054089a50d44692316cb96c
Tory Dannenberg <rfdberg@gmail.com>
2022-03-29 10:24:17 -0600

    Update Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 
 function Header() {
   return (
   <header>
-    Header
+    <div className="logo-container"></div>
+    <nav>
+      <Link to="/home">Home</Link> | {" "}
+      <Link to="/ratings">Ratings</Link>
+    </nav>
   </header>
   );
 }
