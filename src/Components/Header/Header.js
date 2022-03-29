3f2ebd5f5d2f660992d9d1f48e5722869516964c
Tory Dannenberg <rfdberg@gmail.com>
2022-03-29 16:30:54 -0600

    Add initial css for header and overall app

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
   return (
-    <header>
-      <div className="logo-container">BBQ App 🐷</div>
-      <nav>
+    <header className="header-component">
+      <nav className="inital-nav">
         <Link to="/home">Home</Link> | <Link to="/ratings">Ratings</Link>
+      </nav>
+      <h1 className="logo-container">🐷 BBQ App 🐷</h1>
+      <nav className="second-nav">
+        <Link to="/login">Log In</Link> | <Link to="/signup">Sign Up</Link>
       </nav>
     </header>
   );
--- src/Components/Ratings/Ratings.js
+++ src/Components/Ratings/Ratings.js
@@ -1,8 +1,9 @@
 const Ratings = (props) => {
   return (
-    <div className="">
+    <div className="ratings-card">
       <h1>{props.restaurantName}</h1>
       <p>What I Ate: {props.mealChoice}</p>
+      <p>Location: {props.location}</p>
       <p>Date Visited: {props.dateVisited}</p>
       <p>Rating: {props.mealRating}</p>
       <p>Description: {props.experienceDescription}</p>
