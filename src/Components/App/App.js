d75ce757bcbcd9eeb581db5c4db051b54ce38045
Tory Dannenberg <rfdberg@gmail.com>
2022-03-31 14:08:12 -0600

    Fixed some things from peer review

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,25 +4,7 @@ import "./App.css";
 import Header from "../Header/Header";
 import Form from "../Form/Form";
 import RatingsContainer from "../RatingsContainer/RatingsContainer";
-
-const mockRestaurants = [
-  {
-    mealChoice: "Brisket Meal",
-    restaurantName: "AJs Pit BBQ",
-    location: "Denver, CO",
-    dateVisited: "2022-03-10",
-    mealRating: "7.6",
-    experienceDescription: "Really good Texas style bbq.",
-  },
-  {
-    mealChoice: "Pulled Pork Sandwich",
-    restaurantName: "Pee Wees",
-    location: "Blacksburg, VA",
-    dateVisited: "2007-03-03",
-    mealRating: "8.9",
-    experienceDescription: "My favorite place at college, gone too soon",
-  },
-];
+import { mockRestaurants } from "../../assets/mock-data";
 
 class App extends Component {
   constructor(props) {
--- src/Components/Ratings/Ratings.js
+++ src/Components/Ratings/Ratings.js
@@ -1,12 +1,14 @@
+import "./Ratings.css";
+
 const Ratings = (props) => {
   return (
     <div className="ratings-card">
       <h1>{props.restaurantName}</h1>
-      <p>What I Ate: {props.mealChoice}</p>
-      <p>Location: {props.location}</p>
-      <p>Date Visited: {props.dateVisited}</p>
-      <p>Rating: {props.mealRating}</p>
-      <p>Description: {props.experienceDescription}</p>
+      <p><span className="restaurant-bold-text">What I Ate: </span>{props.mealChoice}</p>
+      <p><span className="restaurant-bold-text">Location: </span> {props.location}</p>
+      <p><span className="restaurant-bold-text">Date Visited: </span> {props.dateVisited}</p>
+      <p><span className="restaurant-bold-text">Rating: </span> {props.mealRating}</p>
+      <p><span className="restaurant-bold-text">Restaurant: </span> {props.experienceDescription}</p>
     </div>
   );
 };
